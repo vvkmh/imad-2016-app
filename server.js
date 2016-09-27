005ecccd5927038820ee33b3cd6bed761df45111
vvkmh <vvkmh007@gmail.com>
2016-09-28 01:24:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/smaple05.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.sendFile(path.join(__dirname, 'ui', 'smaple05.jpg'));
 });
 
 
