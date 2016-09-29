f47e797492941b964c4130acf4b3345d259964cc
vvkmh <vvkmh007@gmail.com>
2016-09-30 00:17:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles{
-    
-};
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
