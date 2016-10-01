b94b9ee5651dd647fd5fa15a09c6759221a45379
vvkmh <vvkmh007@gmail.com>
2016-10-02 01:29:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-var img = getElementById('fb');
+var img = document.getElementById('fb');
 img.onclick = function() {
     window.location.href = 'http://facebook.com/vvkmh/';
 };
