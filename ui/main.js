84e39d5bb15345bc3c2dba51952812044806f7ca
vvkmh <vvkmh007@gmail.com>
2016-10-02 01:05:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+
+var img = getElementById('fb');
+img.src = 'image.png';
+img.onclick = function() {
+    window.location.href = 'http://putyourlocationhere/';
+};
+document.body.appendChild(img);
