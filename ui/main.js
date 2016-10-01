8f772d7a1a3adfd365b797c1fe2ec5e8001f483b
vvkmh <vvkmh007@gmail.com>
2016-10-02 01:39:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,15 @@ img.onclick = function() {
     window.location.href = 'http://facebook.com/vvkmh/';
 };
 document.body.appendChild(img);
+
+var img = document.getElementById('tw');
+img.onclick = function() {
+    window.location.href = 'http://twitter.com/vvkmh/';
+};
+document.body.appendChild(img);
+
+var img = document.getElementById('fb');
+img.onclick = function() {
+    window.location.href = 'https://plus.google.com/+VivekMohanvvkmh';
+};
+document.body.appendChild(img);
