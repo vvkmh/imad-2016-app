6482071bd9b8fd70571529482662d6ba0c6d0dd9
vvkmh <vvkmh007@gmail.com>
2016-10-02 01:44:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,2 @@
 console.log('Loaded!');
 
-var img = document.getElementById('fb');
-img.onclick = function() {
-    window.location.href = 'http://facebook.com/vvkmh/';
-};
-document.body.appendChild(img);
-
-var img = document.getElementById('tw');
-img.onclick = function() {
-    window.location.href = 'http://twitter.com/vvkmh/';
-};
-document.body.appendChild(img);
-
-var img = document.getElementById('fb');
-img.onclick = function() {
-    window.location.href = 'https://plus.google.com/+VivekMohanvvkmh';
-};
-document.body.appendChild(img);
