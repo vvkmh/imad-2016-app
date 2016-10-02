04b3e29acc0666cd8bd78b5f496ee7be64ac732f
vvkmh <vvkmh007@gmail.com>
2016-10-02 17:07:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,7 @@
 console.log('Loaded!');
 
 
-var fb=document.getElementById('fb');
-fb.onclick= function () {alert('Are you sure want to leave the site?')};
 
-var tw=document.getElementById('tw');
-tw.onclick= function () {confirm ("Are you sure want to leave the site?")};
-
-var go=document.getElementById('go');
-go.onclick= function () {alert('Are you sure want to leave the site?')};
 
 function myFunction() {
     return "Write something clever here...";
