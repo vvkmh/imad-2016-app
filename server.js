9f77683767954223da47af90644e4ea3a3ebd911
vvkmh <vvkmh007@gmail.com>
2016-10-03 01:45:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,20 +76,20 @@ function articletemplate (data){
 `;
             return htmltemplate;
 }
-                    
-app.get('/:Name', function (req , res) {
-  var Name = req.params.Name;
-  res.send(articletemplate(articles[Name]));
-});
-
-app.get('/', function (req, res) {
+ 
+ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter=0;
-app.get('/counter', function(req, res){
+app.get('/counter', function(req, res) {
     counter = counter = 1;
     res.send(counter.toString()); 
+});
+
+app.get('/:Name', function (req , res) {
+  var Name = req.params.Name;
+  res.send(articletemplate(articles[Name]));
 });
 
 app.get('/ui/main.js', function (req, res) {
