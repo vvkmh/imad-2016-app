05edf7033ec4f6ec01a42444418d9a6e9ce99059
vvkmh <vvkmh007@gmail.com>
2016-10-02 16:30:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var articles={
     heading: 'Speech By Ratan Tata',
     date: '30 Sept 2016',
     content: `
-        
+                <img src="http://i.forbesimg.com/media/lists/people/ratan-tata_416x416.jpg"class="img-frame" />
+                
                 Don't just have career or academic goals. Set goals to give you a balanced, successful life. Balanced means ensuring your
                 health, relationships, mental peace are all in good order. There is no point of getting a promotion on the day of your
                 breakup. There is no fun in driving a car if your back hurts. Shopping is not enjoyable if your mind is full of tensions.
@@ -27,7 +28,7 @@ var articles={
     heading: 'Narendra Modi',
     date: '02 Oct 2016',
     content: `
-                <img src="http://4.bp.blogspot.com/-iUWT-UOY9Sg/VUmWMcVojxI/AAAAAAAADnA/7i63gjuAhMM/s1600/Narendra%2BModi.jpg"class="img-namo" />
+                <img src="http://4.bp.blogspot.com/-iUWT-UOY9Sg/VUmWMcVojxI/AAAAAAAADnA/7i63gjuAhMM/s1600/Narendra%2BModi.jpg"class="img-frame" />
         
                 Full name of 'Narendra Modi' is Narendra Damodardas Modi. He was born on 17 September 1950 in Vadnagar, Mehsana district, Bombay State (present day Gujarat). His father's name was Damodardas Mool Chand Modi and mother's name was Hiraben. He was born into a middle class family. At the age of 13, he was engaged with jasoda Ben Chaman Lal and when they were married, he was just 17.
 
