7f42fbcc9a261c9c473b38dd3496c01b325b2d71
vvkmh <vvkmh007@gmail.com>
2016-10-04 01:02:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ var submit=document.getElementById('submit_butn');
 submit.onclick= function(){
     
    var names=[ 'name1', 'name2', 'name3'];
-   var lsit = '';
+   var list = '';
    for( var i=0; i< names.length; i++){
        list +='<li>' +names[i] + '</li>';
    }
